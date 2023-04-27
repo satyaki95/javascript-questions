1c356cf7486e085b1e73ee152c36b66cd862199e
satyaki95 <satyaki95@outlook.com>
2023-04-27 09:29:48 +0530

    update

--- DSA in javascript/String/reverse2.js
+++ DSA in javascript/String/reverse2.js
@@ -1,5 +1,5 @@
 let str  = "Satyaki Saha";
 
 let str2 = str.split(" ");
-str2 = str2.map((item)=> item.split('').reverse().join().toString().replace(/,/g,''));
+str2 = str2.map((item)=> item.split('').reverse().join().toString().replace(/,/g,'')).toString().replace(/,/g," ");
 console.log(str2);
---
+++ javascript concept/Object/abc.js
@@ -0,0 +1,43 @@
+function maximizeProduct(arr) {
+  let n = arr.length;
+
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    let s1 = arr[i];
+    let len1 = arr[i].length;
+
+    let unmap = new Map();
+    for (let c of s1) {
+      if (unmap.has(c)) {
+        unmap.set(c, unmap.get(c) + 1);
+        console.log('hi');
+      }
+      unmap.set(c, 1);
+      
+    }
+
+    for (let j = i + 1; j < n; j++) {
+      let s2 = arr[j];
+      let len2 = arr[j].length;
+
+      let flag = false;
+
+      for (let k = 0; k < s2.length; k++) {
+        if (unmap.has(s2[k])) {
+          flag = true;
+          break;
+        }
+      }
+
+      if (flag == false) {
+        result = Math.max(result, len1 * len2);
+      }
+    }
+  }
+
+  console.log(result);
+}
+
+let arr = ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"];
+let result = maximizeProduct(arr);
